599c0520621297e721d5bbb18c5c29217810147b
zach <winchelzachary@gmail.com>
2023-10-17 21:41:56 -0400

    changed border style in style.css

--- sript.js
+++ sript.js
@@ -3,6 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
